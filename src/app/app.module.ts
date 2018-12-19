f224676740bd5b67fa8f35c7cf9eb69838a62457
Animesh Rawat <animesh-r@HCL.COM>
2018-12-19 17:31:56 +0530

    project level optimization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { FileReaderComponent } from './file-reader/file-reader.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [FileReaderComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
